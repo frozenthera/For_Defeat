78e680e6d6fcad9fb24e457341b34ec12d9385a6
junseo <junjun5477@gmail.com>
2023-01-07 00:34:28 +0900

    add skillobject moving

--- for_defeat/Assets/Scripts/Skill/SkillObjects/SlashObject.cs
+++ for_defeat/Assets/Scripts/Skill/SkillObjects/SlashObject.cs
@@ -6,5 +6,17 @@ public class SlashObject : MonoBehaviour
 {
     public GameObject origin;
     public GameObject target;
+    [SerializeField] private float speed;
+    private Vector3 dir;
+
+    private void Start()
+    {
+        dir = (target.transform.position - origin.transform.position).normalized;
+    }
+
+    private void Update()
+    {
+        transform.position += speed * dir * Time.deltaTime;
+    }
     
 }
